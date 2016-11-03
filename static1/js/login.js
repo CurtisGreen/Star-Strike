ed550130d263ce0a6b466972f86d23b894fd9d73
greencur000 <greencur000@tamu.edu>
2016-11-03 12:15:34 -0500

    Update login.js

--- static1/js/login.js
+++ static1/js/login.js
@@ -1,6 +1,6 @@
 // Deal with logging in.
 
-function login() {
+function login() {    //TODO: Change for our menu/login
   var userId = $('#user_id_input').val();
   if (userId && userId != "") {
     socket.emit('login', {
@@ -8,12 +8,12 @@ function login() {
     });
   }
 
-  socket.on('login_ok', function(msg) {
+  socket.on('login_ok', function(msg) {   //TODO: replace with running the game
     $('#login_error').html("");
     startChat(userId);
   });
 
-  socket.on('login_fail', function() {
+  socket.on('login_fail', function() {    //TODO: change to different element/console log/error
     $('#login_error').html("Login Failed.");
   });
 }
