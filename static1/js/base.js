ebc2b2ff383dc6081b74c21efb396b22cadb2919
Curtis Green <greencur000@compute-linux1.cs.tamu.edu>
2016-11-07 16:25:39 -0600

    More cleanup

--- static1/js/base.js
+++
@@ -1,7 +0,0 @@
-// Setting up global variables and page element event handlers.
-
-// socket becomes a global variable (within the webpage) representing the
-// WebSocket connection.
-socket = io();
-
-
