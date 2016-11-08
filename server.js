e2023c00ba89f80599c494230facc174f6761515
Jose Ramos <antoniotexas@compute-linux1.cs.tamu.edu>
2016-11-08 10:14:59 -0600

    fixed

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ io.sockets.on('connection', function(conn) {
 
 // Listen on a high port.
 
-var port = 12134;
+var port = 12139;
 
 
 server.listen(port, function() {
--- static1/base.js
+++ static1/base.js
@@ -1,5 +1,5 @@
--// Setting up global variables and page element event handlers.		
- -		
- -// socket becomes a global variable (within the webpage) representing the		
- -// WebSocket connection.		
- -socket = io();
+// Setting up global variables and page element event handlers.		
+ 		
+ // socket becomes a global variable (within the webpage) representing the		
+ // WebSocket connection.		
+ socket = io();
